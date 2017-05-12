977041336a87cad98648214f72b01606361a7104
ilievv <dmxyyyy@gmail.com>
2017-05-13 00:15:11 +0300

    cors added

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+
 const config = require('./config')
 const mailer = require('./mailer')
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
